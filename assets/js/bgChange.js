384acb67220905e4eac229b8f5d245396b562bc9
Phonoxia <maevebollmann@gmail.com>
2026-08-11 17:24:59 +0200

    feat: Background switching is now enabled Added JS implementation of changing backgrounds. NoScript-compatible.

---
+++ assets/js/bgChange.js
@@ -0,0 +1,48 @@
+const timeToBgChange = 20000;
+let bgInterval;
+window.onload = function () {
+    init();
+}
+
+//Initialize Changing of Backgrounds
+function init() {
+    ["mousemove", "keydown", "scroll", "touchstart"].forEach(event=>{document.addEventListener(event, bgChangeTimer);});
+}
+
+//Changes BG after timeToBgChange, only when no Input was detected.
+function bgChangeTimer() {
+    clearInterval(bgInterval);
+    bgInterval = setInterval(() => {
+        changeBG();
+    }, timeToBgChange);
+}
+
+//Change BG to new version.
+function changeBG() {
+    var currentActive = document.getElementsByClassName("bgActive")[0];
+    if(!currentActive) {
+        currentActive = document.getElementsByClassName("bg")[0];
+    }
+
+    var newActive = currentActive.nextElementSibling;
+    if(!newActive) {
+        newActive = document.getElementsByClassName("bg")[0];
+    }
+    
+    displayBG(currentActive, newActive);
+}
+
+//Display the chosen BG. If no new background is passed, displays the currentActive.
+function displayBG(currentActive, newActive = false) {
+    if(!newActive) {
+        currentActive.classList.add("bgActive");
+        currentActive.style.opacity = "1";
+        return;
+    }
+    currentActive.style.opacity = 0;
+    setTimeout(() => {
+        currentActive.classList.remove("bgActive");
+        newActive.classList.add("bgActive");
+        newActive.style.opacity = "1";
+    }, 800);
+}
--- index.php
+++ index.php
@@ -8,21 +8,23 @@ include 'assets/php/functions.php';
     <head>
         <?php include 'assets/php/head.php'?>
         <title>Project Steam</title>
-        <link rel="preload" as="image" href="assets/images/psbg.webp" fetchpriority="high">
+        <link rel="preload" as="image" href="/assets/images/bgs/bg1.jpg" fetchpriority="high">
         <style>
         <?php 
         lightMode($_SESSION['lightMode']);
         language($_SESSION['lang']);
         ?>
         </style>
+        <script src="/assets/js/bgChange.js" defer></script>
     </head>
     <body>
         <?php nav('home'); ?>
         <header>
-            <!-- This is designed to allow future integration of rotating Backgrounds. -->
             <div class="bgContainer">
-                <div class="bg"></div>
-                <!--<div class="bg"></div>-->
+                <div class="bg bgActive" id="bg1" style="opacity: 1;"></div>
+                <div class="bg" id="bg2"></div>
+                <div class="bg" id="bg3"></div>
+                <div class="bg" id="bg3"></div>
             </div>
             <div class="textContainer">
                 <h1>Project Steam</h1>
